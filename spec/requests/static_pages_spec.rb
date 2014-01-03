35af98adb3b789b2bc75317e1cbc63cb140ddecc
cximran <cximran@gmail.com>
2014-01-02 23:53:09 -0500

    Finishing static pages content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,17 +12,26 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_title("Home")
     end
-  describe "Help Page" do
-  it "should have the content 'This is the Help page'" do
-  	visit '/static_pages/help'
-  	expect(page).to have_content('Help Page here!')
- 	 end
   end
-describe "About Page" do 
-	it "should have the content 'About Us'" do
-		visit '/static_pages/about'
-expect(page).to have_content ('About Us')
-end
-end
-end
+  describe "Help Page" do
+    it "should have the content 'This is the Help page'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help Page here!')
+    end
+    it "should have title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+
+    describe "About Page" do 
+      it "should have the content 'About Us'" do
+        visit '/static_pages/about'
+        expect(page).to have_content ("About Us")
+      end
+      it "should have title 'About Us'" do
+        visit '/static_pages/about'
+        expect(page).to have_title("About")
+      end
+    end
+  end
 end
